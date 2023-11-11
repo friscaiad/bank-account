3e148285a3930b01f2e5fe272a839c1b201d5249
friscaiad <friscadewanti53@gmail.com>
2023-11-11 17:41:23 +0700

    "tes up"

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,2 +1,81 @@
-package PACKAGE_NAME;public class bankAccount {
+public class BankAccount {
+    private String accountNumber;
+    private double balance;
+    private String customerName;
+    private String customerEmail;
+    private String customerPhone;
+
+    // Constructor
+    public BankAccount(String accountNumber, double balance, String customerName, String customerEmail, String customerPhone) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.customerPhone = customerPhone;
+    }
+
+    // Getter dan Setter
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+    // Method untuk deposit
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit successful. New balance: $" + balance);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    // Method untuk withdraw
+    public void withdraw(double amount) {
+        if (amount > 0) {
+            if (balance >= amount) {
+                balance -= amount;
+                System.out.println("Withdrawal successful. New balance: $" + balance);
+            } else {
+                System.out.println("Saldo tidak mencukupi");
+            }
+        } else {
+            System.out.println("Invalid withdrawal amount.");
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Membuat objek BankAccount
+        BankAccount myAccount = new BankAccount(
+                "123456",
+                1000.0,
+                "John Doe",
+                "john.doe@example.com",
+                "123-456-7890");
+
+        // Menampilkan informasi awal
+        System.out.println("Initial balance: $" + myAccount.getBalance());
+
+        // Melakukan deposit
+        myAccount.deposit(500.0);
+
+        // Melakukan withdraw
+        myAccount.withdraw(200.0);
+
+        // Melakukan withdraw dengan saldo tidak mencukupi
+        myAccount.withdraw(1500.0);
+
+        // Menampilkan informasi akhir
+        System.out.println("Final balance: $" + myAccount.getBalance());
     }
 }
